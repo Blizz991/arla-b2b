0d38f1580c350ae9ec2ee73c4dd4ae2b455f73e1
Blizz991 <martinhblizz@gmail.com>
2019-10-27 22:29:28 +0100

    Split everything into separate views

--- public/js/variables.js
+++ public/js/variables.js
@@ -3,4 +3,23 @@ var currTabIndex = 0;
 var tabCount = 0;
 var tabsInstance;
 var progressStep;
+//Eventually everything should be handled as views, including the forms
+var viewList = {
+    "views": {
+        "initialView": {
+            "name": ""
+        },
+        "simpleJourneyView": {
+            "name": ""
+        },
+        "advancedJourneyView": {
+            "name": ""
+        },
+        "resultsView": {
+            "name": ""
+        }
+    }
+}
+var currentView = viewList[0];
 var progressTrackerEl = new ldBar("#progressTracker");
+
